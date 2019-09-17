5a85ed0b7eb1a0bc7cc330e18f5846e7b1121037
gelj <378720480@qq.com>
2019-09-17 15:04:21 +0800

    modify

---
+++ src/dongtaiguihua/Fib.java
@@ -0,0 +1,20 @@
+package dongtaiguihua;
+
+/**
+ * 斐波那契数列 0 1 2 3 5 8 13。。。
+ * 记忆化搜索
+ */
+public class Fib {
+    public static void main(String[] args) {
+        System.out.println(fib(6));
+    }
+    public static int fib(int n){
+        if (n == 0){
+            return 0;
+        }
+        if (n == 1){
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+}
